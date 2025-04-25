2c155016a9fd3b3950d3e79217c502ad14259d4a
saivarunguntur <104142062+saivarunguntur@users.noreply.github.com>
2025-04-25 16:27:59 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config({ path: './config.env' });
+
+const connectDB = async () => {
+  try {
+    if (process.env.NODE_ENV === 'test') {
+      var dbName = 'test';
+    } else {
+      var dbName = 'Production';
+    }
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      dbName
+    });
+
+    console.log(`MongoDB Connected to ${dbName} DB...`);
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
